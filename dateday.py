b895fcd2877e5129600364c2ef5e7bf8de1e8d30
amlanjena <67965157+amlanjena@users.noreply.github.com>
2020-07-22 01:27:21 +0530

    Add files via upload

---
+++ dateday.py
@@ -0,0 +1,10 @@
+import datetime 
+import calendar 
+
+def findDay(date): 
+	born = datetime.datetime.strptime(date, '%d %m %Y').weekday() 
+	return (calendar.day_name[born]) 
+
+# Driver program 
+date = '21  07  2020'
+print(findDay(date))
